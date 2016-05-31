78a6b46242270760703e9f0e97b4b602e3e298a8
Samuel Horwitz <sam@kargo.com>
2016-05-31 14:06:49 -0400

    fixing up target searching with regards to a group-internal coord being looked for on the canvas

--- src/shapes/object.class.js
+++ src/shapes/object.class.js
@@ -935,6 +935,16 @@
       return object;
     },
 
+    isDescendantOfGroup: function(ancestor) {
+        var isDescendant = false;
+        this.bubbleThroughGroups(function(g) {
+            if (g === ancestor) {
+                isDescendant = true;
+            }
+        });
+        return isDescendant;
+    },
+
     /**
      * Executes given function for each parent group all the way up
      * @param {Function} callback

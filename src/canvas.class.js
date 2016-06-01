23682d9eafc4abebeabf82f9c3d7224ca761db00
Samuel Horwitz <sam@kargo.com>
2016-06-01 17:32:24 -0400

    better check within target searching

--- src/canvas.class.js
+++ src/canvas.class.js
@@ -1131,7 +1131,7 @@
             }
           }
 
-          if (subTarget || !(target instanceof fabric.Group)) {
+          if (subTarget || !(target instanceof fabric.Group && target.subTargetCheck)) {
             break;
           }
         }
